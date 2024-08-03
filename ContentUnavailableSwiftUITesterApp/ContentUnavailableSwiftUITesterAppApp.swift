58746023d6104febb1f0cfbcd274331d67130f4f
Kavinda Dilshan <kdtechniques@gmail.com>
2024-08-04 00:05:14 +0530

    Initial Commit

---
+++ ContentUnavailableSwiftUITesterApp/ContentUnavailableSwiftUITesterAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContentUnavailableSwiftUITesterAppApp.swift
+//  ContentUnavailableSwiftUITesterApp
+//
+//  Created by Mr. Kavinda Dilshan on 2024-08-04.
+//
+
+import SwiftUI
+
+@main
+struct ContentUnavailableSwiftUITesterAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
